6750a0be7027e194449818030a256ba18184b7cd
Juan Almeida <juanelojga@gmail.com>
2025-10-30 19:31:33 -0500

    Integrate Tailwind CSS and i18n support
    - Add `@astrojs/tailwind` integration and configure Tailwind.
    - Implement dynamic language-based routing with `/[lang]/` structure.
    - Modify `Layout.astro` to include localization and structured header/footer.
    - Update project dependencies to include Tailwind CSS.
    - Redirect root URL to default locale (`/en`).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import { étapes } from './constantes';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-  },
+    defaultLocale: "en",
+    locales: ["en", "es"],
+  }
 });
