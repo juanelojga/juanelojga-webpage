71a9cecc01822166bb0731858fb79e286691e89d
Juan Almeida <juanelojga@gmail.com>
2025-11-06 15:40:23 -0500

    Remove deprecated `@astrojs/image` dependency and clean up unused development dependencies in `package-lock.json` (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.juanelojga.com',
-  integrations: [tailwind(), sitemap(), image(), partytown({
-    config: { forward: ['dataLayer.push', 'gtag'] }
-  })],
+  integrations: [
+    tailwind(), 
+    sitemap(), 
+    partytown({
+      config: { forward: ['dataLayer.push', 'gtag'] }
+    })
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
