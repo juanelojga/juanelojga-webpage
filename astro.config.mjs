3cb1ae11668a9023e853fa9aa812539f8424a86c
Juan Almeida <juanelojga@gmail.com>
2025-11-06 14:51:07 -0500

    Add `@astrojs/partytown` dependency to `package-lock.json` and update development dependencies for improved project configuration. (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanelojga.com',
-  integrations: [tailwind(), sitemap(), image()],
+  integrations: [tailwind(), sitemap(), image(), partytown({
+    config: { forward: ['dataLayer.push', 'gtag'] }
+  })],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
